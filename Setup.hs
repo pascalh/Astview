2537fa697e16e41bc9ecedfb16ff98e766829603
Sebastian Menge <menge@ls10pc13.(none)>
2009-11-18 14:36:46 +0100

    better function names for presentation

--- Setup.hs
+++ Setup.hs
@@ -5,6 +5,9 @@ import Distribution.Simple.Setup (ConfigFlags (..))
 import Distribution.PackageDescription (emptyHookedBuildInfo,HookedBuildInfo(..))
 import Language.Haskell.HsColour (hscolour,Output(CSS))
 import Language.Haskell.HsColour.Colourise (defaultColourPrefs)
+import Control.Monad
+import Data.Maybe
+import Data.List
 
 main :: IO ()
 main = defaultMainWithHooks hooks
@@ -17,36 +20,67 @@ myPreConf args cf = do
   makedocs 
   return emptyHookedBuildInfo
 
+-- read template file with markers, call replaceOrEcho for each marker
 makedocs :: IO ()
 makedocs = do
   putStr "Generating custom html documentation... "
-  tmpl <- readFile "data/astview-tmpl.html"
-  let ls = lines tmpl
-  ls' <- mapM replaceOrEcho ls
+  file <- readFile "data/astview-tmpl.html"
+  replaced <- mapM replaceOrEcho (lines file)
   putStrLn " done."
-  let ls'' = concat ls'
-  writeFile "data/astview.html" (unlines ls'')
+  writeFile "data/astview.html" (unlines . concat $ replaced)
   return ()
 
 
 -- echoes the current line, or, if mymatch succeeds:
 -- replaces the line with colourized haskell code.
 replaceOrEcho :: String -> IO [String]
-replaceOrEcho s = if not $ mymatch s 
+replaceOrEcho s = if not $ match s 
   then return [s]
   else do
-    putStr $ (exex s)++" "
-    ex <- readFile ("data/"++(exex s)++".hs")
-    let ls = lines $ hscolour CSS defaultColourPrefs False True (exex s) False ex 
-    return (["<!-- Example "++(exex s)++" follows: -->"]
-           ++ ls
-           ++ ["<!-- Example "++(exex s)++" done. -->"])
+    putStr $ (extract s)++" "
+    file <- readFile ("data/"++(extract s)++".hs")
+    let replacement = lines $ hscolour CSS defaultColourPrefs False True (extract s) False file
+    return (["<!-- Example "++(extract s)++" follows: -->"]
+           ++ replacement
+           ++ ["<!-- Example "++(extract s)++" done. -->"])
 
+
+-- interface that delegates to various implementations:
 
 -- recognizes Template marker of the form "%%asdf%%"
-mymatch :: String -> Bool
-mymatch s = take 2 s == "%%" && take 2 (reverse s) == "%%"
+match :: String -> Bool
+match = match0 "%%"
 
 --extracts the filename from the marker
-exex :: String -> String
-exex s = let remainder = (drop 2 s) in reverse (drop 2 (reverse remainder) )
+extract :: String -> String
+extract = extract1 "%%"
+
+--------  Implementations  --------------
+
+match0 :: String -> String -> Bool
+match0 p s = take 2 s == p && take 2 (reverse s) == p
+
+match1 :: String -> String -> Bool
+match1 p = liftM2 (&&) 
+             (help p) 
+             (help p . reverse) 
+  where help q = (q ==) . (take (length q))
+
+match2 :: String -> String -> Bool
+match2 p s = p `isSuffixOf` s && (reverse p) `isPrefixOf` s
+
+extract1 :: String -> String -> String
+extract1 p s = let remainder = (drop (length p) s) in reverse (drop (length p) (reverse remainder) )
+
+extract2 :: String -> String -> String
+extract2 p s = reverse (drop (length p) (reverse (drop (length p) s)))
+
+
+extract3 :: String -> String -> String
+extract3 p s = reverse . drop (length p) $ reverse $ drop (length p) s
+
+
+extract4 :: String -> String
+extract4 = help . reverse . help
+  where help :: String -> String
+        help = fromJust . (stripPrefix "%%%")
