3f6ba0742058d06e934a5f1cefdd36392a6c1b26
Pascal Hof <pascal.hof@tu-dortmund.de>
2012-12-16 13:16:55 +0100

    improved first monoid to determine matching source location

--- src/Language/Astview/SourceLocation.hs
+++ src/Language/Astview/SourceLocation.hs
@@ -34,14 +34,29 @@ findSrcLoc :: Int -- ^line
            -> Int -- ^row 
            -> [SrcLocation] 
            -> Maybe SrcLocation
-findSrcLoc l r srcLocs
-  | elem (SrcPosition l r) srcLocs = Just (SrcPosition l r) 
-  | otherwise        = 
-   getFirst $ (First $ find (\s -> contains s (SrcPosition l r)) srcLocs) `mappend` 
-              (First $ find equalsLine srcLocs)  where
-    equalsLine :: SrcLocation -> Bool
-    equalsLine (SrcPosition ll _) = ll ==l
-    equalsLine _ = False 
+findSrcLoc l r srcLocs = 
+  getFirst $ foldr (<>) mempty [ sameElement l r srcLocs 
+                               , smallestSpanContaining l r srcLocs 
+                               , equalsLine l srcLocs
+                               ]
+
+-- |returns the source position given by line and row, 
+-- iff it is element of the list
+sameElement :: Int -> Int -> [SrcLocation] -> First SrcLocation
+sameElement l r = First . find (\s -> s==(SrcPosition l r))
+
+-- |the source location which is the smallest one containing the position
+-- given by line and row
+smallestSpanContaining :: Int -> Int -> [SrcLocation] -> First SrcLocation
+smallestSpanContaining l r =
+  First . smallestSpan . filter (\s -> contains s (SrcPosition l r)) 
+
+-- |the first source location which has given line
+equalsLine :: Int -> [SrcLocation] -> First SrcLocation
+equalsLine l = First . find p where
+  p :: SrcLocation -> Bool
+  p (SrcPosition ll _) = ll == l
+  p _                  = False
 
 -- |returns the value which is associated with given source location
 getAssociatedValue :: Maybe SrcLocation -> [(SrcLocation,[a])] -> [a] 
@@ -50,6 +65,13 @@ getAssociatedValue _ [] = []
 getAssociatedValue (Just s) ((loc,v):xs)
   | s == loc  = v
   | otherwise = getAssociatedValue (Just s) xs
+
+
+-- |returns the smallest span, i.e. the span with does not contain any other.
+smallestSpan :: [SrcLocation] -> Maybe SrcLocation
+smallestSpan []     = Nothing
+smallestSpan (x:xs) = 
+  Just $ foldr (\s acc -> if contains acc s then s else acc) x xs
 
 -- |returns whether first argument source location contains the second one
 contains :: SrcLocation -> SrcLocation -> Bool
