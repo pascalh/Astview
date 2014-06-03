18483c8ba111e0682df1a7c5fd4a15c1f00cac8f
Pascal Hof <pascal.hof@tu-dortmund.de>
2014-06-03 14:17:24 +0200

    using set instead list

--- src/core/Language/Astview/Pathlist.hs
+++ src/core/Language/Astview/Pathlist.hs
@@ -1,6 +1,6 @@
 {- |
 One source location can be represented by multiple nodes in a abstract
-syntax tree.  A pathlist stores a source location and the associated tree 
+syntax tree.  A 'Pathlist' stores a source location and the associated tree 
 positions. 
 
 This data structure is specialized to find the smallest source location
@@ -17,38 +17,30 @@ module Language.Astview.Pathlist
   ,ins
   ,fromList
   ) where
-import Data.List(sort)
+import qualified Data.Set as S
 import Language.Astview.Language (SrcLocation(..))
 
 -- |a pathlist stores a source location and paths to all subtrees annotated
 -- with the source location
 data PathList 
   = Empty -- ^ the empty list
-  | PathList  SrcLocation [[Int]] 
+  | PathList  SrcLocation (S.Set [Int]) 
     -- ^ a source location and their associated tree positions
-
-instance Eq PathList where
-  Empty             == Empty             = True
-  (PathList s1 ps1) == (PathList s2 ps2) = s1 == s2 && sort ps1 == sort ps2
-  _                 == _                 = False
-
-instance Show PathList where
-  show Empty = "<>"
-  show (PathList s p) = "<"++show s++" @ "++show p++">"
+  deriving (Show,Eq)
 
 singleton :: (SrcLocation,[Int]) -> PathList
 singleton (x,p) = ins (x,p) Empty 
 
 ins :: (SrcLocation,[Int]) -> PathList -> PathList 
-ins (s   ,p   ) Empty = PathList s [p] 
+ins (s   ,p   ) Empty = PathList s $ S.singleton p 
 ins (sNew,pNew) (PathList s ps) 
-  | s == sNew  = PathList s $ pNew:ps
+  | s == sNew  = PathList s $ S.insert pNew ps
   | s > sNew   = singleton (sNew,pNew)
   | otherwise  = PathList s ps 
 
 toList :: PathList -> [(SrcLocation,[Int])]
 toList Empty           = []
-toList (PathList s ps) = map (\p -> (s,p)) ps
+toList (PathList s ps) = map (\p -> (s,p)) $ S.toList ps
 
 -- |Creates a PathList from a given list if pairs.
 -- Thus, it returns the smallest source location and all paths to operations
