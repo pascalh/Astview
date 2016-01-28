ab67c44d4007372a90398670cbaeea0fef559184
Pascal Hof <pascal.hof@tu-dortmund.de>
2016-01-28 10:10:58 +0100

    a first step to generating arbitrary terms, which are annotated with source spans

--- test/DataTree.hs
+++ test/DataTree.hs
@@ -1,4 +1,4 @@
-{-| This module contains test cases for the functions creating abstract 
+{-| This module contains test cases for the functions creating abstract
 syntax trees defined in module 'Language.Astview.DataTree'. -}
 module DataTree(testDataTree) where
 
@@ -8,23 +8,25 @@ import Test.Tasty.QuickCheck hiding (label)
 import Data.Tree(Tree(..))
 import Control.Monad(liftM,liftM2,liftM3)
 import Data.Foldable (foldrM)
+import Data.List(sort)
 import Data.Generics (Data)
 import Data.Typeable(Typeable,typeOf)
-import Language.Astview.DataTree 
+import Control.Monad.Omega(diagonal)
+import Language.Astview.DataTree
 import Language.Astview.Language
 
 
 propIgnoreInt :: TestTree
 propIgnoreInt = testProperty "removing leafs with int values" p where
- 
+
   p (TermUnit t) = act t === exped t
 
   act :: Term () -> Tree String
   act = fmap label . ast . dataToAstIgnoreByExample (const Nothing) (1::Int)
 
   exped :: Term () -> Tree String
-  exped = removeSubtrees (\t -> isNumber t || isEmpty t) . fmap label . ast . dataToAstSimpl 
-  
+  exped = removeSubtrees (\t -> isNumber t || isEmpty t) . fmap label . ast . dataToAstSimpl
+
   isEmpty :: Tree String -> Bool
   isEmpty = null . rootLabel
 
@@ -33,19 +35,19 @@ propIgnoreInt = testProperty "removing leafs with int values" p where
   isNumber (Node xs@(_:_)  _) = all (\x -> elem x ['0'..'9']) xs
   isNumber _                  = False
 
--- * 
+-- *
 
 testDataTree :: TestTree
 testDataTree = testGroup "data to ast" [testsBasic,propIgnoreInt] where
 
 testsBasic :: TestTree
-testsBasic = testGroup "Basic data to ast transformations"  
+testsBasic = testGroup "Basic data to ast transformations"
                        [mkProp b f | b <- [True,False] , f <- [True,False]] where
 
   mkProp ::Bool -> Bool -> TestTree
   mkProp b f = testProperty name p where
 
-    name = (if b then " " else "remove annotations ") ++ 
+    name = (if b then " " else "remove annotations ") ++
            (if f then "flattened" else "")
 
     p :: TermUnit -> Property
@@ -57,32 +59,44 @@ actual :: Bool  -- ^ should the term contain annotations=
        -> Term () -- ^ input term
        -> Tree String
 actual b f = fmap label . ast . mkFlat . mkAst  where
- 
-  mkFlat :: Ast -> Ast 
+
+  mkFlat :: Ast -> Ast
   mkFlat = if f then flatten else id
 
-  mkAst :: Term () -> Ast 
-  mkAst = if b then dataToAstSimpl 
-               else dataToAstIgnoreByExample (const Nothing) () 
+  mkAst :: Term () -> Ast
+  mkAst = if b then dataToAstSimpl
+               else dataToAstIgnoreByExample (const Nothing) ()
 
 -- * a toy language
 
 data Term ann
-  = EmptyLeaf 
+  = EmptyLeaf
   | Leaf ann String
   | Number ann Int
   | Nested ann (Term ann)
   | BinaryBranch ann (Term ann) (Term ann)
   | List ann [Term ann]
-  deriving (Show,Data,Typeable)
+  deriving (Show,Data,Typeable,Functor)
 
-newtype TermUnit = TermUnit {term :: Term () } 
+newtype TermUnit = TermUnit {term :: Term () }
 
 instance Show TermUnit where
   show = show . term
 
 instance Arbitrary TermUnit where
   arbitrary = liftM TermUnit arbitrary
+
+newtype TermSrcloc = TermSrcloc { termSrcLoc :: Term SrcLocation}
+
+instance Show TermSrcloc where
+  show = show . termSrcLoc
+
+instance Arbitrary TermSrcloc where
+  arbitrary = do
+    TermUnit t <- arbitrary
+    l <- arbitrary
+    t' <- arbitrarySrcLocs l t
+    return $ TermSrcloc t'
 
 -- * creating the expected trees
 
@@ -94,7 +108,7 @@ expected _ _ EmptyLeaf = Node "EmptyLeaf" []
 expected b _ (Leaf _ s)  = annotate b $ Node "Leaf" [Node s []]
 expected b _ (Number _ n) = annotate b $ Node "Number" [Node (show n) []]
 expected b f (Nested _ t) = annotate b $ Node "Nested" [expected b f t]
-expected b f (BinaryBranch _ t1 t2) = 
+expected b f (BinaryBranch _ t1 t2) =
   annotate b $ Node "BinaryBranch" [expected b f t1,expected b f t2]
 expected b False (List _ ts) = annotate b $ Node "List" [ts' ts] where
 
@@ -109,8 +123,8 @@ expected b True (List _ ts) = annotate b $ Node "List" [Node lbl (map (expected 
   lbl = "["++replicate (length ts - 1) ',' ++ "]"
 
 annotate :: Bool -> Tree String -> Tree String
-annotate b (Node l cs) 
-  | b         = let unit = Node "()" [] 
+annotate b (Node l cs)
+  | b         = let unit = Node "()" []
                 in Node l $ unit : cs
   | otherwise = Node l cs
 
@@ -121,17 +135,18 @@ instance Arbitrary ann => Arbitrary (Term ann) where
 
 arbitraryTerm :: Arbitrary a => Int -> Gen (Term a)
 arbitraryTerm 0 = oneof [return EmptyLeaf, arbitraryString, arbitraryInt]
-arbitraryTerm n | n > 0 =
-  oneof [ arbitraryNested n, arbitraryBinary n, arbitraryList n]
+arbitraryTerm n
+  | n > 0 = oneof [ arbitraryNested n, arbitraryBinary n, arbitraryList n]
+  | n < 0 = return EmptyLeaf
 
 arbitraryString :: Arbitrary a => Gen (Term a)
 arbitraryString = liftM2 Leaf arbitrary (listOf1 arbitraryChar) where
-  
+
   arbitraryChar :: Gen Char
   arbitraryChar = elements $ ['A'..'Z']++['a'..'z']
 
 arbitraryInt :: Arbitrary a => Gen (Term a)
-arbitraryInt = liftM2 Number arbitrary arbitrary 
+arbitraryInt = liftM2 Number arbitrary arbitrary
 
 arbitraryNested :: Arbitrary a => Int -> Gen (Term a)
 arbitraryNested n = liftM2 Nested arbitrary (arbitraryTerm (n-1))
@@ -147,25 +162,54 @@ arbitraryList n = do
   ann <- arbitrary
   case m of
     0 -> return $ List ann []
-    1 -> liftM (\t -> List ann [t]) (arbitraryTerm (n-1)) 
+    1 -> liftM (\t -> List ann [t]) (arbitraryTerm (n-1))
     _ -> do
       ns <- splits m n
       ts <- foldrM (\n acc -> do {t <- arbitraryTerm n ;  return $ t:acc}) [] ns
       return $ List ann ts
 
--- |returns an arbitrary pair (x1,x2) such that x1 + x2 == n
+-- |split n returns an arbitrary pair (x1,x2) such that x1 + x2 == n
 split :: Int -> Gen (Int,Int)
 split n = do
-  x <- elements [0..n] 
+  x <- elements [0..n]
   return (x,n-x)
 
 splits :: Int -> Int -> Gen [Int]
-splits 2 n = do 
-  (x1,x2) <- split n  
+splits 2 n = do
+  (x1,x2) <- split n
   return [x1,x2]
 splits m n = do
   (x1,x2) <- split n
   xs <- splits (m-1) x1
   return (x2:xs)
+
+-- |given a term and a source span arbitrarySrcLocs annotates the term
+-- with source spans, such that the span at the root of every subterm t
+-- surrounds the spans of every successor of t.
+arbitrarySrcLocs :: SrcLocation -> Term () -> Gen (Term SrcLocation)
+arbitrarySrcLocs _ EmptyLeaf = return EmptyLeaf
+arbitrarySrcLocs l (Leaf _ str) = return $ Leaf l str
+arbitrarySrcLocs l (Number _ n) = return $ Number l n
+arbitrarySrcLocs l (Nested _ t) = do
+  l' <- sublocation l
+  t' <- arbitrarySrcLocs l' t
+  return $ Nested l' t'
+
+-- |sublocation loc returns a arbitrary source span loc' which is
+-- surrounded by loc
+sublocation :: SrcLocation -> Gen SrcLocation
+sublocation loc = do
+  return loc
+
+-- |returns n source positions which are in the given source span
+positionsInSpan :: Int -> SrcLocation -> [SrcLocation]
+positionsInSpan n = sort . take n . diagonal . locsPerLine where
+
+  locsPerLine :: SrcLocation -> [[SrcLocation]]
+  locsPerLine (SrcSpan bl bc el ec)
+    | bl == el = [[position bl c | c <- [bc..ec]]]
+    | bl > el = []
+    | bl < el = [position bl c | c <- [bc..]] :
+                locsPerLine (SrcSpan (bl+1) 1 el ec)
 
 
